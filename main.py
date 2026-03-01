16d50962d14c99433d650a6d13d8ac690e04bc16
Jackson-91-Henrique <jackson91henrique@gmail.com>
2026-03-01 17:54:02 -0300

    complete chapter1

---
+++ main.py
@@ -0,0 +1,62 @@
+"""
+
+New repository
+
+name
+story
+
+description
+my masterpiece
+
+publico
+
+create repository
+
+https://github.com/jackson-91-henrique/story.git
+
+git remote add origin https://github.com/jackson-91-henrique/story.git
+git branch -M main
+git push -u origin main
+
+#
+cd 001-udemy-101-aulas/day-70/501
+git init
+git status
+git add [+ nome]
+git add . [vai tudo]
+
+touch chapter1.txt
+It was the age of wisdom, it was the age of foolisheness.
+"""
+# "/c/dev/programas/PyCharm 2025.1.3.1/bin/pycharm64.exe" chapter1.txt
+# $ find "/c" -iname "pycharm64.exe" 2>/dev/null | head -n 20
+#
+# /c/dev/programas/PyCharm 2025.1.3.1/bin/pycharm64.exe
+# /c/Users/PC/Desktop/pycharm64.exe
+#
+#
+# 4) Criar o comando pycharm (alias correto)
+#
+# Abra o .bashrc:
+#
+# nano /c/Users/PC/.bashrc
+#
+#
+# Cole esta linha (uma linha só):
+#
+# alias pycharm='"/c/dev/programas/PyCharm 2025.1.3.1/bin/pycharm64.exe"'
+#
+# Salve e saia:
+#
+# Ctrl + O (Write Out) → Enter
+#
+# Ctrl + X (Exit)
+#
+# Aplique:
+#
+#
+# source /c/Users/PC/.bashrc
+#
+# pycharm chapter1.txt
+#
+# se atentar ao caminho da cmd
